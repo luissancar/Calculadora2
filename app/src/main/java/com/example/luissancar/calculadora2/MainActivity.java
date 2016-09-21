bcf04fb9411dd832f72057f530dd9caa87f3a1ad
luissancar <luissancar@yahoo.es>
2016-09-21 12:37:16 +0200

    Segundo

--- app/src/main/java/com/example/luissancar/calculadora2/MainActivity.java
+++ app/src/main/java/com/example/luissancar/calculadora2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.luissancar.calculadora2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void pulsado(View view) {
+
+        TextView tv=(TextView)findViewById(view.getId());
+        Toast.makeText(this,tv.getText().toString(),Toast.LENGTH_SHORT).show();
     }
 }
